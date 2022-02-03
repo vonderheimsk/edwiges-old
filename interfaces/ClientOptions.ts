02c3ab753d82b05deec7d3f902f9c234c17fcad5
Heimsk <64988539+Heimsk@users.noreply.github.com>
2022-02-03 18:53:18 -0300

    Update ClientOptions.ts

--- interfaces/ClientOptions.ts
+++ interfaces/ClientOptions.ts
@@ -1,6 +1,6 @@
 export default interface ClientOptions {
     gateway_version?: number;
-    first_shard_id?: number
+    first_shard_id?: number;
     last_shard_id?: number;
     shards?: number;
     connectOneShardAtTime?: boolean;
